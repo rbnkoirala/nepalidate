e547c643036a80d7941f0dbd1f570175273e9220
rbnkoirala <rbnkoirala@hotmail.com>
2023-12-03 07:18:01 +0000

    Add conversion from english to nepali and nepali to english

--- nepalidate/nepali_date/doctype/nepali_date_demo/nepali_date_demo.js
+++ nepalidate/nepali_date/doctype/nepali_date_demo/nepali_date_demo.js
@@ -1,7 +1,25 @@
 frappe.ui.form.on("Nepali Date Demo", {
     refresh(frm) {
-        add_nepali_date_picker(frm, "nepali_date");   
-        add_nepali_date_picker(frm, "nepali_date_convert");  
-         
+        add_nepali_date_picker(frm, "nepali_date");   // if you dont want to add nepali date only
+        add_nepali_date_picker(frm, "nepali_date_convert", "english_date_convert");   //  if you want to add nepali date and convert to english date      
+        add_nepali_date_picker(frm, "nepali_date_time_convert", "english_datetime_convert");  // convert in time field
+
+        add_nepali_date_picker(frm, "nepali_date_convert_one", "english_date_convert_one"); 
+       
+        
+        
     },
+    english_date_convert(frm) {
+        convert_ADBS(frm, "english_date_convert", "nepali_date_convert");  
+    },
+    english_datetime_convert(frm) {
+        convert_ADBS(frm, "english_datetime_convert", "nepali_date_time_convert");  
+    },    
+    english_date_convert_one(frm) {
+        convert_ADBS(frm, "english_date_convert_one", "nepali_date_convert_one");  
+    },
+    english_datetime_convert_one(frm) {
+        convert_ADBS(frm, "english_datetime_convert_one", "nepali_date_convert_one");  
+    }
 });
+
--- nepalidate/public/js/nepali.datepicker.custom.js
+++ nepalidate/public/js/nepali.datepicker.custom.js
@@ -1,14 +1,72 @@
-function add_nepali_date_picker(frmField, nepali_date_field) {
+
+function add_nepali_date_picker(frmField, nepali_date_field , english_date_convert_field) {   
     if (typeof $.fn.nepaliDatePicker !== 'undefined') {
-        frmField.fields_dict[nepali_date_field].df.fieldtype = "Data";
         $(frmField.fields_dict[nepali_date_field].input).nepaliDatePicker({
             ndpYear: true,
             ndpMonth: true,
-            ndpYearCount: 10
-        });     
+            ndpYearCount: 10,
+            onChange: function(e) {
+                var date_format_converted = new Date(e.ad);
+                if (frmField.fields_dict[english_date_convert_field] && frmField.fields_dict[english_date_convert_field].df.fieldtype == "Datetime") {
+                    var time = "00:00:00";
+                    date_format_converted = date_format_converted.toISOString().split("T")[0] + " " + time;
+                } 
+                frappe.model.set_value(frmField.doctype, frmField.docname, nepali_date_field, e.bs);
+                frappe.model.set_value(frmField.doctype, frmField.docname, english_date_convert_field, date_format_converted);
+            }
+        }); 
 
-        frmField.fields_dict[nepali_date_field].df.placeholder = "Enter your data here"; 
-        frmField.fields_dict[nepali_date_field].df.max_length = 10;
-        frmField.fields_dict[nepali_date_field].refresh();        
+        // set max length for nepali date field
+        $(frmField.fields_dict[nepali_date_field].input).attr("maxlength", 10);
+
+        $(frmField.fields_dict[nepali_date_field].input).on("change", function() {
+            var nepali_date = $(this).val();
+            frappe.model.set_value(frmField.doctype, frmField.docname, nepali_date_field, nepali_date);
+            console.log('hello');
+            if (/^[0-9-]+$/.test(nepali_date) && nepali_date.length == 10) {
+                var date = nepali_date.split("-");
+                var year = date[0];
+                var month = date[1];
+                var day = date[2];
+                if (year.length == 4 && month.length == 2 && day.length == 2) {
+                    frappe.model.set_value(frmField.doctype, frmField.docname, nepali_date_field, nepali_date);
+                } else {
+                    frappe.model.set_value(frmField.doctype, frmField.docname, nepali_date_field, "");
+                    $(frmField.fields_dict[nepali_date_field].input).focus();
+                }
+            } else {
+                frappe.model.set_value(frmField.doctype, frmField.docname, nepali_date_field, "");
+                $(frmField.fields_dict[nepali_date_field].input).focus();
+            }
+        });        
+
+    }
+   
+    
+}
+
+
+function convert_ADBS(frmField, english_date_field, nepali_date_field) {
+    
+    if (frmField.fields_dict[english_date_field].df.fieldtype == "Datetime" || frmField.fields_dict[english_date_field].df.fieldtype == "Date") {
+    
+        var english_date = frmField.fields_dict[english_date_field].value;
+
+        if (english_date && english_date.includes && english_date.includes("GMT")) {
+            var date_format_converted = new Date(english_date);
+            english_date = date_format_converted.toISOString().split("T")[0];
+        }
+
+       
+        if (english_date && english_date.includes && english_date.includes(" ")) {
+            english_date = english_date.split(" ")[0];
+        }
+      
+        var nepali_date = NepaliFunctions.AD2BS(english_date, "YYYY-MM-DD", "YYYY-MM-DD");
+        frappe.model.set_value(frmField.doctype, frmField.docname, nepali_date_field, nepali_date);
+    } else {
+        frappe.model.set_value(frmField.doctype, frmField.docname, nepali_date_field, "");
     }
 }
+     
+
