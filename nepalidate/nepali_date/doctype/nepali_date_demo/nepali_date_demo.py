6700fc82c79f5e74635da04156cea7e2ee11276f
Rabin Koirala <64308806+rbnkoirala@users.noreply.github.com>
2023-12-19 17:26:37 +0545

    Update nepali_date_demo.py

--- nepalidate/nepali_date/doctype/nepali_date_demo/nepali_date_demo.py
+++ nepalidate/nepali_date/doctype/nepali_date_demo/nepali_date_demo.py
@@ -1,5 +1,7 @@
 # Copyright (c) 2023, Rabin Koirala and contributors
 # For license information, please see license.txt
 
+from frappe.model.document import Document
+
 class NepaliDateDemo(Document):
 	pass
